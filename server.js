01586da946fa31d1c6c4763c917340e0b54236fe
Aleksey Lovchikov <svdmusic@gmail.com>
2015-04-13 09:48:23 +0300

    add button to footer

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 var express = require('express');
 var app = express();
-var instagram = require('instagram-node').instagram();
+var ig = require('instagram-node').instagram();
 var port = Number(process.env.PORT || 3000);
 var secrets = require('./secrets.json');
+var bodyParser = require('body-parser');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-instagram.use({
+ig.use({
     client_id: secrets.client_id,
     client_secret: secrets.client_secret
 });
 
 app.get('/', function(req, res) {
-    instagram.media_popular(function(err, medias, remaining, limit) {
+    ig.media_popular(function(err, medias, remaining, limit) {
         res.render('pages/index', { grams: medias });
         console.log(medias);
     });
+});
+
+app.post('/', function(req, res){
+    console.log(req.body.user.name);
+    console.log(req.body.user.email);
 });
 
 app.listen(port, function(err) {
